85b24694bc912bd348bf0ca09eee7b0f0b6e8dd0
Yuto Teranishi <yt05271992@yahoo.co.jp>
2022-02-28 19:28:13 +0900

    controller ,views　諸々更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards' , to: 'cards#index'
+
+  get 'cards/add'
+  post 'cards/add'
+
+  get 'cards/edit/:id',to:'cards#edit'
+  patch 'cards/edit/:id',to:'cards#edit'
+  get 'cards/delete/:id' , to:'cards#delete'
+  get 'cards/:id' , to:'cards#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
